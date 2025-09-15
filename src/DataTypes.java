a55493044916e048fe0ff3f8078c00a5f8cbe82c
SorEgo <146003878+SwornEgo@users.noreply.github.com>
2025-09-15 11:53:01 -0400

    Fixed DataTypes.java, and checked off items in README.md

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,11 +1,20 @@
 public class DataTypes {
     // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(int[] numbers) {
-        int s = 0; // variable to accumulate the sum in!
+        long s = 0; // variable to accumulate the sum in!
         // below is a "foreach" loop which iterates through numbers
-        for (int x : numbers) {
+        for (long x : numbers) {
             s += x;
         }
         return s;
+    }
+
+    public static void main(String[] args) {
+        int[] number = new int[5];
+        for (int x = 1; x < 5; x++) {
+            number[x-1] = x*5;
+        }
+
+        System.out.println(sum(number));
     }
 }
